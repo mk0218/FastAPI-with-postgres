fafdd141db278683d1f928f415991bb48dc109e0
mkkim <minamay1@gmail.com>
2021-03-23 21:56:51 +0900

    Add data models

---
+++ api/api/database.py
@@ -0,0 +1,19 @@
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_USERNAME = environ.get('DATABASE_USERNAME', 'postgres')
+DATABASE_PASSWORD = environ.get('DATABASE_PASSWORD', 'postgres')
+DATABASE_URL = environ.get('DATABASE_URL', 'db')
+DATABASE_PORT = environ.get('DATABASE_PORT', 5432)
+DATABASE_NAME = environ.get('DATABASE_NAME', 'db')
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}\
+                            :{DATABASE_PASSWORD}@{DATABASE_URL}\
+                            :{DATABASE_PORT}/{DATABASE_NAME}"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ api/api/models.py
@@ -0,0 +1,58 @@
+from sqlalchemy import Column, DateTime, Integer, String
+
+from .database import Base
+
+
+class Person(Base):
+    __tablename__ = "person"
+
+    person_id = Column(Integer, primary_key=True)
+    gender_concept_id = Column(Integer, nullable=False)
+    birth_datetime = Column(DateTime, nullable=False)
+    race_concept_id = Column(Integer, nullable=False)
+    ethnicity_concept_id = Column(Integer, nullable=False)
+
+
+class VisitOccurence(Base):
+    __tablename__ = "visit_occurence"
+
+    visit_occurrence_id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, nullable=False)
+    visit_concept_id = Column(Integer, nullable=False)
+    visit_start_datetime = Column(DateTime, nullable=False)
+    visit_end_datetime = Column(DateTime, nullable=False)
+
+
+class ConditionOccurrence(Base):
+    __tablename__ = "condition_occurrence"
+
+    person_id = Column(Integer, nullable=False)
+    condition_concept_id = Column(Integer, primary_key=True)
+    condition_start_datetime = Column(DateTime, nullable=False)
+    condition_end_datetime = Column(DateTime, nullable=False)
+    visit_occurrence_id = Column(Integer, nullable=False)
+
+
+class DrugExposure(Base):
+    __tablename__ = "drug_exposure"
+
+    person_id = Column(Integer, nullable=False)
+    drug_concept_id = Column(Integer, nullable=False)
+    drug_exposure_start_datetime = Column(DateTime, nullable=False)
+    drug_exposure_end_datetime = Column(DateTime, nullable=False)
+    visit_occurence_id = Column(Integer, nullable=False)
+
+
+class Concept(Base):
+    __tablename__ = "concept"
+
+    concept_id = Column(Integer, nullable=False)
+    concept_name = Column(String, nullable=False)
+    domain_id = Column(String, nullable=False)
+
+
+class Death(Base):
+    __tablename__ = "death"
+
+    person_id = Column(Integer, nullable=False)
+    death_date = Column(DateTime, nullable=False)
